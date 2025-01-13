fff3dcb303240e79f787be71e7dfe565956523f0
Guilherme Radicchi <guilhermeradicchi@hotmail.com>
2025-01-13 10:19:21 -0300

    trilha de python

--- 12 - Desenvolvimento fullstack com Django/mysite/polls/admin.py
+++ 12 - Desenvolvimento fullstack com Django/mysite/polls/admin.py
@@ -1,13 +1,3 @@
 from django.contrib import admin
 
-from .models import Choice, Question
-
-
-@admin.register(Choice)
-class ChoiceAdmin(admin.ModelAdmin):
-    pass
-
-
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    pass
+# Register your models here.
